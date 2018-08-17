2ae05a2eb1151a885ab1388f65b3cb24452c9fe6
Damian_Kreisich <kreisichd@gmail.com>
2018-08-17 16:35:25 +0200

    Created base files for models and dbConnections

---
+++ ConsoleWorkGraphic/DataBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+
+namespace ConsoleWorkGraphic
+{
+    class DataBase
+    {
+        private MySqlConnection dataBase;
+
+        DataBase(String connection)
+        {
+            dataBase = new MySqlConnection(connection);
+        }
+    }
+}
--- ConsoleWorkGraphic/Program.cs
+++ ConsoleWorkGraphic/Program.cs
@@ -4,9 +4,10 @@ namespace ConsoleWorkGraphic
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
